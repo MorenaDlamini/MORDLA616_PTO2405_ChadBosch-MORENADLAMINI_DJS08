bae76af13acf277d9edf340704b9d3f31c3d8684
MorenaDlamini <dlaminimorena@gmail.com>
2025-03-09 19:57:34 +0200

    Footer page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * A simple footer component that displays a copyright notice.
+ *
+ * @component
+ * @returns {JSX.Element} - Returns a footer element with a copyright notice.
+ */
+export default function Footer() {
+    return <footer>&#169; 2022 #VANLIFE</footer>;
+}
