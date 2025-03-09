0c72d9c641dbc9371db7108d1e47b440088c7990
MorenaDlamini <dlaminimorena@gmail.com>
2025-03-09 19:48:54 +0200

    Main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Ensure this import is correct
+import './index.css';
 
+/**
+ * The entry point of the React application.
+ * Renders the App component into the root DOM element.
+ *
+ * @module main
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
