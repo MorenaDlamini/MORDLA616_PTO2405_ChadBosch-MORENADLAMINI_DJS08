d222c6800e60613d23f190b9aa6e0d1993dda3f5
MorenaDlamini <dlaminimorena@gmail.com>
2025-03-09 19:57:19 +0200

    Hostlayout page

---
+++ src/components/HostLayout.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+
+/**
+ * A layout component for the host section of the application.
+ * It includes navigation links for the host dashboard, income, vans, and reviews.
+ *
+ * @component
+ * @returns {JSX.Element} - Returns a fragment containing navigation links and an `Outlet` for rendering child routes.
+ */
+export default function HostLayout() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616",
+    };
+
+    return (
+        <>
+            <nav className="host-nav">
+                <NavLink
+                    to="."
+                    end
+                    style={({ isActive }) => (isActive ? activeStyles : null)}
+                >
+                    Dashboard
+                </NavLink>
+                <NavLink
+                    to="income"
+                    style={({ isActive }) => (isActive ? activeStyles : null)}
+                >
+                    Income
+                </NavLink>
+                <NavLink
+                    to="vans"
+                    style={({ isActive }) => (isActive ? activeStyles : null)}
+                >
+                    Vans
+                </NavLink>
+                <NavLink
+                    to="reviews"
+                    style={({ isActive }) => (isActive ? activeStyles : null)}
+                >
+                    Reviews
+                </NavLink>
+            </nav>
+            <Outlet />
+        </>
+    );
+}
